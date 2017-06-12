f514518a5a25c611af89e5614a84ff6d5d5c0653
Obinna <sho.jnr@gmail.com>
2017-06-12 14:15:47 -0400

    applied pep8 formatting to python file

--- vagrant/newsdata/newsdb.py
+++ vagrant/newsdata/newsdb.py
@@ -1,38 +1,36 @@
 #! /usr/bin/env python
-import psycopg2 as p
+import psycopg2
 
 
 DBNAME = "news"
 
-def connect(database_name="news"):
-    try:
-        db = psycopg2.connect("DBNAME={}".format(database_name))
-        cursor = db.cursor()
-        return db, cursor
-    except:
-        print("Error Messages")
+
         
-
 def getPopularArticles():
-    db, cursor = connect()
 
-        popular_articles = '''select articles.title, count(*) as views
+    db = psycopg2.connect(database=DBNAME)
+    c= db.cursor()
+    popular_articles = '''select articles.title, count(*) as views
            from articles, log
            where log.path = concat('/article/', articles.slug)
            group by articles.title
            order by views DESC
            limit 3'''
-    
     c.execute(popular_articles)
-    print("Most popular articles:")
-    for (title, count) in c.fetchall():
-           print("    {} - {} views".format(title, count))
+    rows = c.fetchall()
+    print "\n 1. What are the most popular three articles of all time?"
+    print "Show me the data for #1! \n"
+    for row in rows:
+        print " ", row[0], "-", row[1], "views"
+    db.close()
+    
            
 
            
 def getPopularAuthors():
-    db, cursor = connect()
-
+    
+    db = psycopg2.connect(database=DBNAME)
+    c= db.cursor()
     popular_authors = '''select authors.name as author, count(log.path) as views
            from articles, log, authors
            where log.path = concat('/article/', articles.slug)
@@ -40,29 +38,43 @@ def getPopularAuthors():
            group by authors.name
            order by views DESC
            limit 3'''
-    
     c.execute(popular_authors)
-    print("Most popular authors:")
-    for (title, count) in c.fetchall():
-           print("    {} - {} views".format(name, count))
+    rows = c.fetchall()
+    print "\n 2. Who are the authors with the most article views?"
+    print "Show me the data for #2! \n"
+    for row in rows:
+        print " ", row[0], "-", row[1], "views"
+    db.close()
            
 
 def getStatusDays():
-    db, cursor = connect()
+    
+    db = psycopg2.connect(database=DBNAME)
+    c= db.cursor()
 
     StatusDays = '''select to_char(date, 'FMMonth FMDD, YYYY'),
-                    err/total as ratio
+                    (err/total * 100) as ratio
                     from (select time::date as date,
                     count(*) as total,
                     sum((status != '200 OK')::int)::float as err
                     from log
                     group by date) as errors
                     where err/total > 0.01;'''
-    
     c.execute(StatusDays)
-    print("Days which more than 1% of requests lead to errors were on:")
-    for (date, count) in c.fetchall():
-           print("    {} - {} views".format(name, count))
+    rows = c.fetchall()
+    print "\n 3. The days with status requests more than 1%?"
+    print "Show me the data for #3! \n"
+    for row in rows:
+        print " ", row[0], "-", row[1], "%"
+        print "\n THE END"
     db.close()
+
+
+
+
+getPopularArticles()
+getPopularAuthors()
+getStatusDays()
+
 
 
